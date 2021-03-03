85de6b3131d5f7fa6b2da7ab0f5d2452a32f5847
Regina Emelyanovich <respase@mail.ru>
2021-03-03 21:00:47 +0300

    Задание 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,24 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    let credit = amount - contribution;
+    let monthlyPercent = percent / 100 / 12;
+    let months = (date.getFullYear() - new Date().getFullYear()) * 12;
+
+    let monthlyPayment = credit * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent) ** months) - 1));
+
+    let totalAmount = (monthlyPayment * months).toFixed(2);
+    console.log(totalAmount);
+
+    return totalAmount; // return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    if (name === "" || name === undefined) {
+    	name = "Аноним";
+    }
+
+    return `Привет, мир! Меня зовут ${name}.`;
 }

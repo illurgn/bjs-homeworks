5520ecf4c8bd6213a8368099fd981ecdabc787a4
Regina Emelyanovich <respase@mail.ru>
2021-03-08 09:36:38 +0300

    задание 2 лекции 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,32 @@
+'use strict';
 //String.prototype.isPalindrome - для задачи №1
+/*String.prototype = isPalindrome();
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+function isPalindrome() {
+    this === this.reverse();
+}*/
+
+//задача №2
+function getAverageMark(marks) { // тесты прошли
+    let sum = 0;
+
+    if (marks.length === 0) {
+		return 0;
+	}
+
+	for (let i = 0; i < marks.length; i++) {
+		sum += marks[i];
+	}
+
+	return Math.round(sum / marks.length);
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+//задача №3
+/*function checkBirthday(birthday) {
+    let now = Date.now();
+    let birthday = Date.now(birthday);
+    let diff = now - birthday; // код для задачи №3 писать здесь
+    let age = Math.trunc(diff / 31536000000);
+
+    return age;
+}*/
